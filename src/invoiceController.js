9d836aab434a6b593ffe1ad550d3219deb872978
Achraf Ghellach <a.ghellach@gmail.com>
2021-06-02 16:20:46 -0400

    fix

--- src/invoiceController.js
+++ src/invoiceController.js
@@ -1,9 +1,6 @@
 import Mongo from './models/_main.js';
 import Validator from './validators/_main.js';
 import Provider from './providers/_main.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import moment from 'moment';
 
 export async function addInvoice(req, res) {
     try {
